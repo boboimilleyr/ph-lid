8976e3b645182edd07ac868e95331dd08d264f07
Kmerge Boi <kpdosdos@gmail.com>
2022-04-28 02:06:30 +0800

    Added dataset generator

--- Data/zource_code.py
+++ Data/zource_code.py
@@ -1,4 +1,4 @@
-from distutils.command.clean import clean
+# from distutils.command.clean import clean
 from os import write
 import re
 import csv
@@ -15,7 +15,6 @@ def splitToSentences(src, des):
         line.strip().split()) > 1 and line.strip()[-1] in ".?!'\""]
 
     # split each paragraph into sentences
-    # sentences = par    -> this is the working version
     sentences = []
     for p in par:
         sen = re.split("[.?!]", p.strip())
@@ -62,7 +61,27 @@ def isSentenceValid(s):
     return False
 
 
+# for reference in case needed
+def splitToSen():
+    """
+    Splits a paragraph into sentences and retains the ending punctuation/s
+    """
+    text = "<lbl>16</lbl> Mitubag si Jesus: May nag-andam og dakong hikay. Daghan siyag dinapit. <lbl>17</lbl> Gipaadto niya ang katabang aron pag-ingon: 'Dali na mo, andam na ang tanan.' <lbl>18</lbl> Apan morag nagsabot ang tanan sa pagpamalibad. Miingon ang usa: 'Mipalit kog uma nga kinahanglang adtoon ug susihon. Pasensya ka na.' <lbl>19</lbl> Miingon sab ang lain: 'Nakapalit kog lima ka parisang baka nga pang-uma ug pasulayon kog daro. Pasensya ka na.' <lbl>20</lbl> Miingon ang usa pa: 'Bag-o kong nakasal busa, dili makaadto?'"
+    sen = [s.strip() for s in re.split(
+        "(\..?|!.?|\?.?|\.\.\..?)", text) if s.strip()]
+
+    sentences = []
+    for i in range(0, len(sen), 2):
+        if(i+1 < len(sen)):
+            sentences.append(sen[i]+sen[i+1])
+
+    return sentences
+
+
 def writeCSV():
+    """
+    Creates the annotated dataset for LID
+    """
     # header of the dataset
     header = ["Text", "Language"]
 
@@ -73,160 +92,130 @@ def writeCSV():
         # writes the header row to the csv
         writer.writerow(header)
 
-        # BIKOL texts
+        # BIKOL texts: add each sentence/line with a label into the dataset
         with open("Bikol.txt", encoding="utf-8") as fbikol:
             bikol = fbikol.read().split("\n")
-            # transfers FIRST 20 paragraphs (note: update this)
-            for i in range(20):
-                # splits the sentences but retains the ending punctuation/s
-                sen = [s.strip() for s in re.split(
-                    "(\..?|!.?|\?.?|\.\.\..?)", bikol[i]) if s.strip()]
-                # adds each sentence and label to the dataset
-                for j in range(0, len(sen), 2):
-                    if(j+1 < len(sen)):
-                        writer.writerow([sen[j]+sen[j+1], "Bikol"])
+            for sentence in bikol:
+                writer.writerow([sentence, "Bikol"])
 
-        # CEBUANO TEXTS
-        with open("Cebuano.txt", encoding="utf-8", newline="") as fceb:
-            ceb = fceb.read().split("\n")
-            # transfer FIRST 350 paragraphs (note: update this)
-            for i in range(350):
-                # splits the sentences but retains the ending punctuation/s
-                sen = [s.strip() for s in re.split(
-                    "(\..?|!.?|\?.?|\.\.\..?)", ceb[i]) if s.strip()]
-                # adds each sentence and label to the dataset
-                for j in range(0, len(sen), 2):
-                    if(j+1 < len(sen)):
-                        writer.writerow([sen[j]+sen[j+1], "Cebuano"])
+        # CEBUANO texts: add each sentence/line with a label into the dataset
+        with open("Cebuano.txt", encoding="utf-8") as fcebuano:
+            cebuano = fcebuano.read().split("\n")
+            for sentence in cebuano:
+                writer.writerow([sentence, "Cebuano"])
 
-        # HILIGAYNON
-        with open("Hiligaynon.txt", encoding="utf-8", newline="") as fhili:
-            hili = fhili.read().split("\n")
-            # transfer FIRST 100 paragraphs (note: update this)
-            for i in range(100):
-                # splits the sentences but retains the ending punctuation/s
-                sen = [s.strip() for s in re.split(
-                    "(\..?|!.?|\?.?|\.\.\..?)", hili[i]) if s.strip()]
-                # adds each sentence and label to the dataset
-                for j in range(0, len(sen), 2):
-                    if(j+1 < len(sen)):
-                        writer.writerow([sen[j]+sen[j+1], "Hiligaynon"])
+        # HILIGAYNON texts: add each sentence/line with a label into the dataset
+        with open("Hiligaynon.txt", encoding="utf-8") as fhiligaynon:
+            hiligaynon = fhiligaynon.read().split("\n")
+            for sentence in hiligaynon:
+                writer.writerow([sentence, "Hiligaynon"])
 
-        # ILOCANO
-        with open("Ilocano.txt", encoding="utf-8", newline="") as filoc:
-            iloc = filoc.read().split("\n")
-            # transfer FIRST 460 paragraphs (note: update this)
-            for i in range(460):
-                # splits the sentences but retains the ending punctuation/s
-                sen = [s.strip() for s in re.split(
-                    "(\..?|!.?|\?.?|\.\.\..?)", iloc[i]) if s.strip()]
-                # adds each sentence and label to the dataset
-                for j in range(0, len(sen), 2):
-                    if(j+1 < len(sen)):
-                        writer.writerow([sen[j]+sen[j+1], "Ilocano"])
+        # ILOCANO texts: add each sentence/line with a label into the dataset
+        with open("Ilocano.txt", encoding="utf-8") as filocano:
+            ilocano = filocano.read().split("\n")
+            for sentence in ilocano:
+                writer.writerow([sentence, "Ilocano"])
 
-        # KAPAMPANGAN
-        with open("Kapampangan.txt", encoding="utf-8", newline="") as fkap:
-            kap = fkap.read().split("\n")
-            # transfer FIRST 900 paragraphs (note: update this)
-            for i in range(900):
-                # splits the sentences but retains the ending punctuation/s
-                sen = [s.strip() for s in re.split(
-                    "(\..?|!.?|\?.?|\.\.\..?)", kap[i]) if s.strip()]
-                # adds each sentence and label to the dataset
-                for j in range(0, len(sen), 2):
-                    if(j+1 < len(sen)):
-                        writer.writerow([sen[j]+sen[j+1], "Kapampangan"])
+        # KAPAMPANGAN texts: add each sentence/line with a label into the dataset
+        with open("Kapampangan.txt", encoding="utf-8") as fkapampangan:
+            kapampangan = fkapampangan.read().split("\n")
+            for sentence in kapampangan:
+                writer.writerow([sentence, "Kapampangan"])
 
-        # TAGALOG
-        with open("Tagalog.txt", encoding="utf-8", newline="") as ftag:
-            tag = ftag.read().split("\n")
-            # transfer FIRST 350 paragraphs (note: update this)
-            for i in range(350):
-                # splits the sentences but retains the ending punctuation/s
-                sen = [s.strip() for s in re.split(
-                    "(\..?|!.?|\?.?|\.\.\..?)", tag[i]) if s.strip()]
-                # adds each sentence and label to the dataset
-                for j in range(0, len(sen), 2):
-                    if(j+1 < len(sen)):
-                        writer.writerow([sen[j]+sen[j+1], "Tagalog"])
+        # PANGASINENSE texts: add each sentence/line with a label into the dataset
+        with open("Pangasinense.txt", encoding="utf-8") as fpangasinense:
+            pangasinense = fpangasinense.read().split("\n")
+            for sentence in pangasinense:
+                writer.writerow([sentence, "Pangasinense"])
+
+        # TAGALOG texts: add each sentence/line with a label into the dataset
+        with open("Tagalog.txt", encoding="utf-8") as ftagalog:
+            tagalog = ftagalog.read().split("\n")
+            for sentence in tagalog:
+                writer.writerow([sentence, "Tagalog"])
+
+        # WARAY texts: add each sentence/line with a label into the dataset
+        with open("Waray.txt", encoding="utf-8") as fwaray:
+            waray = fwaray.read().split("\n")
+            for sentence in waray:
+                writer.writerow([sentence, "Waray"])
 
 
 def main():
     print("hello world")
     # writeCSV()
 
-    # bikol
-    file_bikol_lit = open("Bikol_Literary_Text.txt", encoding='utf-8')
-    splitToSentences(file_bikol_lit, "Bikol-Lit.txt")
-    file_bikol_lit.close()
+    # # bikol
+    # file_bikol_lit = open("Bikol_Literary_Text.txt", encoding='utf-8')
+    # splitToSentences(file_bikol_lit, "Bikol-Lit.txt")
+    # file_bikol_lit.close()
 
-    file_bikol_rel = open("Bikol_Religious_Text.txt", encoding='utf-8')
-    splitToSentences(file_bikol_rel, "Bikol-Rel.txt")
-    file_bikol_rel.close()
+    # file_bikol_rel = open("Bikol_Religious_Text.txt", encoding='utf-8')
+    # splitToSentences(file_bikol_rel, "Bikol-Rel.txt")
+    # file_bikol_rel.close()
 
-    # cebuano
-    file_ceb_lit = open("Cebuano_Literary_Text.txt", encoding='utf-8')
-    splitToSentences(file_ceb_lit, "Cebuano-Lit.txt")
-    file_ceb_lit.close()
+    # # cebuano
+    # file_ceb_lit = open("Cebuano_Literary_Text.txt", encoding='utf-8')
+    # splitToSentences(file_ceb_lit, "Cebuano-Lit.txt")
+    # file_ceb_lit.close()
 
-    file_ceb_rel = open("Cebuano_Religious_Text.txt", encoding='utf-8')
-    splitToSentences(file_ceb_rel, "Cebuano-Rel.txt")
-    file_ceb_rel.close()
+    # file_ceb_rel = open("Cebuano_Religious_Text.txt", encoding='utf-8')
+    # splitToSentences(file_ceb_rel, "Cebuano-Rel.txt")
+    # file_ceb_rel.close()
 
-    # hiligaynon
-    file_hil_lit = open("Hiligaynon_Literary_Text.txt", encoding='utf-8')
-    splitToSentences(file_hil_lit, "Hiligaynon-Lit.txt")
-    file_hil_lit.close()
+    # # hiligaynon
+    # file_hil_lit = open("Hiligaynon_Literary_Text.txt", encoding='utf-8')
+    # splitToSentences(file_hil_lit, "Hiligaynon-Lit.txt")
+    # file_hil_lit.close()
 
-    file_hil_rel = open("Hiligaynon_Religious_Text.txt", encoding='utf-8')
-    splitToSentences(file_hil_rel, "Hiligaynon-Rel.txt")
-    file_hil_rel.close()
+    # file_hil_rel = open("Hiligaynon_Religious_Text.txt", encoding='utf-8')
+    # splitToSentences(file_hil_rel, "Hiligaynon-Rel.txt")
+    # file_hil_rel.close()
 
-    # ilocano
-    file_ilo_lit = open("Ilocano_Literary_Text.txt", encoding='utf-8')
-    splitToSentences(file_ilo_lit, "Ilocano-Lit.txt")
-    file_ilo_lit.close()
+    # # ilocano
+    # file_ilo_lit = open("Ilocano_Literary_Text.txt", encoding='utf-8')
+    # splitToSentences(file_ilo_lit, "Ilocano-Lit.txt")
+    # file_ilo_lit.close()
 
-    file_ilo_rel = open("Ilocano_Religious_Text.txt", encoding='utf-8')
-    splitToSentences(file_ilo_rel, "Ilocano-Rel.txt")
-    file_ilo_rel.close()
+    # file_ilo_rel = open("Ilocano_Religious_Text.txt", encoding='utf-8')
+    # splitToSentences(file_ilo_rel, "Ilocano-Rel.txt")
+    # file_ilo_rel.close()
 
-    # kapampangan
-    file_kap_lit = open("Kapampangan_Literary_Text.txt", encoding='utf-8')
-    splitToSentences(file_kap_lit, "Kapampangan-Lit.txt")
-    file_kap_lit.close()
+    # # kapampangan
+    # file_kap_lit = open("Kapampangan_Literary_Text.txt", encoding='utf-8')
+    # splitToSentences(file_kap_lit, "Kapampangan-Lit.txt")
+    # file_kap_lit.close()
 
-    file_kap_rel = open("Kapampangan_Religious_Text.txt", encoding='utf-8')
-    splitToSentences(file_kap_rel, "Kapampangan-Rel.txt")
-    file_kap_rel.close()
+    # file_kap_rel = open("Kapampangan_Religious_Text.txt", encoding='utf-8')
+    # splitToSentences(file_kap_rel, "Kapampangan-Rel.txt")
+    # file_kap_rel.close()
 
-    # pangasinense
-    file_pan_lit = open("Pangasinense_Literary_Text.txt", encoding='utf-8')
-    splitToSentences(file_pan_lit, "Pangasinense-Lit.txt")
-    file_pan_lit.close()
+    # # pangasinense
+    # file_pan_lit = open("Pangasinense_Literary_Text.txt", encoding='utf-8')
+    # splitToSentences(file_pan_lit, "Pangasinense-Lit.txt")
+    # file_pan_lit.close()
 
-    file_pan_rel = open("Pangasinense_Religious_Text.txt", encoding='utf-8')
-    splitToSentences(file_pan_rel, "Pangasinense-Rel.txt")
-    file_pan_rel.close()
+    # file_pan_rel = open("Pangasinense_Religious_Text.txt", encoding='utf-8')
+    # splitToSentences(file_pan_rel, "Pangasinense-Rel.txt")
+    # file_pan_rel.close()
 
-    # tagalog
-    file_tag_lit = open("Tagalog_Literary_Text.txt", encoding='utf-8')
-    splitToSentences(file_tag_lit, "Tagalog-Lit.txt")
-    file_tag_lit.close()
+    # # tagalog
+    # file_tag_lit = open("Tagalog_Literary_Text.txt", encoding='utf-8')
+    # splitToSentences(file_tag_lit, "Tagalog-Lit.txt")
+    # file_tag_lit.close()
 
-    file_tag_rel = open("Tagalog_Religious_Text.txt", encoding='utf-8')
-    splitToSentences(file_tag_rel, "Tagalog-Rel.txt")
-    file_tag_rel.close()
+    # file_tag_rel = open("Tagalog_Religious_Text.txt", encoding='utf-8')
+    # splitToSentences(file_tag_rel, "Tagalog-Rel.txt")
+    # file_tag_rel.close()
 
-    # waray
-    file_war_lit = open("Waray_Literary_Text.txt", encoding='utf-8')
-    splitToSentences(file_war_lit, "Waray-Lit.txt")
-    file_war_lit.close()
+    # # waray
+    # file_war_lit = open("Waray_Literary_Text.txt", encoding='utf-8')
+    # splitToSentences(file_war_lit, "Waray-Lit.txt")
+    # file_war_lit.close()
 
-    file_war_rel = open("Waray_Religious_Text.txt", encoding='utf-8')
-    splitToSentences(file_war_rel, "Waray-Rel.txt")
-    file_war_rel.close()
+    # file_war_rel = open("Waray_Religious_Text.txt", encoding='utf-8')
+    # splitToSentences(file_war_rel, "Waray-Rel.txt")
+    # file_war_rel.close()
 
 
 if __name__ == "__main__":
